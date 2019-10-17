ce2ef4df460cf776e5c986d6f4c56e66d499be37
TheDryck <rodgersdryck@gmail.com>
2019-10-16 23:37:42 -0500

    Exercise 2 completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,12 +5,31 @@ let layout = document.querySelector("link");
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log("workingg");
+    //console.log("workingg");
     let board = document.getElementById("board");
-    const a = board.querySelectorAll("div");
-    a.forEach((i)=>{
+    const square = board.querySelectorAll("div");
+    square.forEach((i)=>{
         i.className = "square";
-    })
+    });
+    let square_state = true;
+    square.forEach((i) => {
+        i.addEventListener("click", () => {
+            if(!(i.getAttribute("class").includes("X") || i.getAttribute("class").includes("O"))){
+                if(square_state === true){
+                    i.setAttribute("class", "square X");
+                    i.innerHTML = "X";
+                    square_state = false;
+                }
+                else if(square_state === false){
+                    i.setAttribute("class", "square O");
+                    i.innerHTML = "O";
+                    square_state = true
+                }
+            }
+        });
+    });
+
+    let square_array = [];
 });
 /* layout.addEventListener("DOMContentLoaded", function(){
     console.log("jsnjdn");
